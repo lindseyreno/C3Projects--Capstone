52a1bbd93ac2cd1d313f723dd35cf748567a8ef1
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-15 16:27:07 -0700

    added tests for model validations for name to be present and unique

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "model validations" do
+    describe "name validations" do
+      it "requires a name" do
+        category = build :category, name: nil
+
+        category.valid?
+        expect(category.errors.keys).to include(:name)
+      end
+
+      it "requires name to be unique" do
+        create :category
+        category = build :category
+
+        category.valid?
+        expect(Category.count).to be 1
+        expect(category.errors.keys).to include(:name)
+      end
+    end
+  end
 end
