9bfe1c3813a8cab5cf17cebb62bed407c2069b1c
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-15 16:25:24 -0700

    added factories for categories and events

---
+++ spec/factories.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :event do
+    title "Climb for Clean Air Info Session"
+    date "2015-10-24"
+    location "Seattle Bouldering Project, 900 Poplar Pl S, Seattle, WA 98144"
+    url "http://action.lung.org/site/TR?fr_id=12350&amp;pg=entry"
+    email "Elepine@lungmtpacific.org"
+    description "Climb For Clean Air is exclusive mountaineering training."
+  end
+
+  factory :category do
+    name "Sports and Recreation"
+  end
+end
