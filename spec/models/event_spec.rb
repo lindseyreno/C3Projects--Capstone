5addbaea168ef4722a36b41480bb835f8e0a6bff
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-19 09:24:30 -0700

    adds tests for model validations with all passing right now

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "model validations" do
+    describe "presence_of validations" do
+      required_attributes = [:title, :location, :date]
+      required_attributes.each do |attribute|
+        it "requires #{attribute}" do
+          event = build :event
+          event[attribute] = nil
+          event.valid?
+          expect(event.errors.keys).to include(attribute)
+        end
+      end
+    end
+    describe "unique validations" do
+      it "requires a title, date and location combination to be unique" do
+        create :event
+        event = build :event, description: "Climbing!"
+        event.valid?
+        expect(event.errors.keys).to include(:title)
+      end
+
+      it "will persist an event with the title and location the same but the date different" do
+        create :event
+        event = build :event, date: "2015-10-25"
+        event.valid?
+        expect(event.errors.keys).to_not include(:title)
+      end
+    end
+  end
 end
