428665348a7265360f9e83888cfd649a0cce898b
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-19 09:23:50 -0700

    adds model validations for presence of title, date and location and requires the combination of title, date and location to be unique

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ActiveRecord::Base
   has_and_belongs_to_many :categories
+
+  validates_presence_of :title, :location, :date
+  validates :title, uniqueness: { scope: [:date, :location] }
 end
