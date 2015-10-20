440abde71062836747dda9c67eb47f448220fb1a
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-20 09:47:53 -0700

    added has_secure_password and model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates_format_of :email, with: /\w+@\w+\.\w+/, :on => :create
+  validates :password, presence: true, confirmation: true
 end
