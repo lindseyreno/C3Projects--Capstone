9c42a17081911f5b7977ee4ed8997a0938f782bd
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-21 10:02:01 -0700

    added new, create, and destroy actions for signing in and set_user private method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,27 @@
 class SessionsController < ApplicationController
+  before_action :set_user, only: [:create]
+
+  def new; end
+
+  def create
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:errors] = ERRORS[:sign_in_error]
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:messages] = MESSAGES[:successful_sign_out]
+    redirect_to root_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(username: params[:session][:username])
+  end
 end
