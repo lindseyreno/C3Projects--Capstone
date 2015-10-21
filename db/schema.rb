e9bda4926cc25f2ee2a9234a02e79d3706567ca3
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-21 10:17:06 -0700

    added activation_digest, activated and activated_at columns to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020161441) do
+ActiveRecord::Schema.define(version: 20151021171544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,8 +45,11 @@ ActiveRecord::Schema.define(version: 20151020161441) do
     t.text     "username"
     t.text     "email"
     t.text     "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.text     "activation_digest"
+    t.boolean  "activated"
+    t.datetime "activated_at"
   end
 
 end
