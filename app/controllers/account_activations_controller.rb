ba56f3b6391034f114858cda220b54ac4cfee550
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-21 14:28:11 -0700

    added #edit action that activates a user if they click the link and signs them in

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,14 @@
 class AccountActivationsController < ApplicationController
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.update(activated: true)
+      user.update(activated_at: Time.zone.now)
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash[:errors] = ERRORS[:invalid_activation]
+      redirect_to root_path
+    end
+  end
 end
