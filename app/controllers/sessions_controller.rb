fd593ac71f1a72ea77f5a55fdd7465640fc93051
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-21 14:26:17 -0700

    added requirement for users to be activated before a session can be created

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,13 @@ class SessionsController < ApplicationController
 
   def create
     if @user && @user.authenticate(params[:session][:password])
-      session[:user_id] = @user.id
-      redirect_to root_path
+      if @user.activated?
+        session[:user_id] = @user.id
+        redirect_to root_path
+      else
+        flash[:errors] = ERRORS[:unactivated_account]
+        redirect_to root_path
+      end
     else
       flash[:errors] = ERRORS[:sign_in_error]
       redirect_to root_path
