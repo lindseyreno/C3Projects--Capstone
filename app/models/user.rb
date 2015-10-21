c4b030d352d5db005624fd7d9a3c560bfba9a2d3
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-21 10:58:10 -0700

    added #new_token #digest(string) and #create_activation_digest and attr_accessor for activation_token and a before_create for #create_activation_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,25 @@
 class User < ActiveRecord::Base
+  attr_accessor :activation_token
+  before_create :create_activation_digest
   has_secure_password
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates_format_of :email, with: /\w+@\w+\.\w+/, :on => :create
   validates :password, presence: true, confirmation: true
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
