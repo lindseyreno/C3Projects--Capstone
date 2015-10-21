5a5aaa0cef80ce7ab11b644c5573836ddb2f9b3e
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-21 14:30:30 -0700

    adds user edit link and action, sends email to user on create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UserMailer.account_activation(@user).deliver_now
       redirect_to root_path
     else
       flash.now[:errors] = ERRORS[:unsuccessful_signup]
@@ -19,9 +20,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
