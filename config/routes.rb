be3c5f7a11abec0d23c1df569e3e5e2f71983f98
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-22 13:15:27 -0700

    added get and patch route for updating user category preferences

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   post "/sign_in", to: "sessions#create"
   delete "/sign_out", to: "sessions#destroy", as: "sign_out"
 
+  get "/users/:id/edit_preferences", to: "users#edit_preferences", as: "edit_preferences"
+  patch "/users/:id/update_preferences", to: "users#update_preferences", as: "update_preferences"
+
   resources :account_activations, only: [:edit]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
