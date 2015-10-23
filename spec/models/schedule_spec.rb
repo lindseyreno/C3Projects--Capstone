88de58f8c92cfb383a7f2d5600b204cab0fc2cf0
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-23 15:16:27 -0700

    added tests for schedule model validations, tests pass

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Schedule, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "model validations" do
+    describe "name validations" do
+      it "requires a name" do
+        schedule = build :schedule, name: nil
+
+        schedule.valid?
+        expect(schedule.errors.keys).to include(:name)
+      end
+
+      it "requires name to be unique" do
+        create :schedule
+        schedule = build :schedule
+
+        schedule.valid?
+        expect(Schedule.count).to be 1
+        expect(schedule.errors.keys).to include(:name)
+      end
+    end
+
+    describe "description validations" do
+      it "requires a description" do
+        schedule = build :schedule, description: nil
+
+        schedule.valid?
+        expect(schedule.errors.keys).to include(:description)
+      end
+
+      it "requires description to be unique" do
+        create :schedule
+        schedule = build :schedule
+
+        schedule.valid?
+        expect(Schedule.count).to be 1
+        expect(schedule.errors.keys).to include(:description)
+      end
+    end
+  end
 end
