d8fdad53ec2939bd0c6d94d803b7a336334e350b
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-26 15:42:09 -0700

    setup for previewing the newsletter email

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -8,4 +8,10 @@ class UserMailerPreview < ActionMailer::Preview
     UserMailer.account_activation(user)
   end
 
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/newsletter
+  def newsletter
+    user = User.first
+    plan = "everyday"
+    UserMailer.newsletter(user, plan)
+  end
 end
