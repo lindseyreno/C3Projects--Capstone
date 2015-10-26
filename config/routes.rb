f3e77e39d5803f733b8595e94f82ba092d7548cf
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-26 15:41:18 -0700

    fixed style issues

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,12 @@ Rails.application.routes.draw do
   root 'welcome#index'
   resources :users
 
-  get "/sign_in", to: "sessions#new", as: "sign_in"
-  post "/sign_in", to: "sessions#create"
-  delete "/sign_out", to: "sessions#destroy", as: "sign_out"
+  get '/sign_in', to: 'sessions#new', as: 'sign_in'
+  post '/sign_in', to: 'sessions#create'
+  delete '/sign_out', to: 'sessions#destroy', as: 'sign_out'
 
-  get "/users/:id/edit_preferences", to: "users#edit_preferences", as: "edit_preferences"
-  patch "/users/:id/update_preferences", to: "users#update_preferences", as: "update_preferences"
+  get '/users/:id/edit_preferences', to: 'users#edit_preferences', as: 'edit_preferences'
+  patch '/users/:id/update_preferences', to: 'users#update_preferences', as: 'update_preferences'
 
   resources :account_activations, only: [:edit]
   # Example of regular route:
