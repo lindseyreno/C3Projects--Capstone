f71c7a68e46e7107bcae5613e0c1fd1d9c0c861b
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-26 14:32:06 -0700

    added newsletter method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,12 @@ class UserMailer < ApplicationMailer
 
     mail to: user.email, subject: 'Account activation'
   end
+
+  def newsletter(user, plan)
+    @user = user
+    @categories = @user.categories
+    @schedule = Schedule.find(@user.schedule_id).name
+    @plan = plan
+    mail to: user.email, subject: 'My Seattle Events Newsletter'
+  end
 end
