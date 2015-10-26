1d573de569a6d0ff37f500e2eff739bda982cd7d
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-26 14:27:16 -0700

    removed comments and added schedules for newsletters

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,24 +1,35 @@
 # Use this file to easily define all of your cron jobs.
-#
+
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
-# Example:
-#
-set :output, "log/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
 # Learn more: http://github.com/javan/whenever
 
+set :output, 'log/cron_log.log'
+
+# Adds new events to the db from seattlegov citywide
+every :day, :at => '12:15am' do
+  rake 'events:create_seattlegov_citywide'
+end
+
+# Sends the newsletter everyday to the users with that schedule
 every :day, :at => '12:30am' do
-  rake "events:create_seattlegov_citywide"
+  rake 'email:everyday'
+end
+
+# Sends the newsletter on Wednesday's to the users who want events for the weekend
+every :wednesday, :at => '12:45am' do
+  rake 'email:weekend'
+end
+
+# Sends the newsletter on Sunday's for users who want events for the week
+every :sunday, :at => '1am' do
+  rake 'email:week'
+end
+
+# Sends the newsletter on Sunday's for the users who want events every two weeks
+
+# Sends the newsletter on the first of the month for users who want events for the month
+every :month do
+  rake 'email:month'
 end
