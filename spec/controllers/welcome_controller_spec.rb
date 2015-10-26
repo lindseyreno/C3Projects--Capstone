455fa95926ec3950dbc1001050bcfe37050209c0
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-25 21:22:15 -0700

    changed double quotes to single quotes

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,19 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
-  describe "Actions" do
-    describe "GET #index" do
-      it "responds successfully with an HTTP 200 status code" do
-        get :index
+  describe 'GET #index' do
+    it 'responds successfully with an HTTP 200 status code' do
+      get :index
 
-        expect(response).to be_success
-        expect(response).to have_http_status(200)
-      end
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
 
-      it "renders the index template" do
-        get :index
-        expect(response).to render_template("index")
-      end
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template('index')
     end
   end
 end
