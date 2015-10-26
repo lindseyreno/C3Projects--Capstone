c9d9291722343a90d3cc6925852d952069961dae
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-26 14:30:24 -0700

    added scopes to get schedules by name

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,4 +3,10 @@ class Schedule < ActiveRecord::Base
 
   validates :name, presence: true
   validates :description, presence: true
+
+  scope :everyday, -> { where(:name => 'Every Day') }
+  scope :weekend, -> { where(:name => 'Just the Weekend') }
+  scope :week, -> { where(:name => 'This Week') }
+  scope :two_weeks, -> { where(:name => 'Every Two Weeks') }
+  scope :month, -> { where(:name => 'This Month') }
 end
