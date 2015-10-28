4fa47b6a9e0bf34119ec1ffdf5744ff6d07f41a5
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-28 11:23:45 -0700

    added conditional statement to handle both sign in and sign up on the welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    else
+      @user = User.new
+    end
+    @categories = Category.all
     @events = Event.todays_events
   end
 end
