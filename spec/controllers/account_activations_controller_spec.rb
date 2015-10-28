74cb16d21248c86752c735ef36f2122aa5e92f18
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-28 11:25:33 -0700

    added tests for #edit, tests pass

--- spec/controllers/account_activations_controller_spec.rb
+++ spec/controllers/account_activations_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe AccountActivationsController, type: :controller do
+  describe 'GET #edit' do
+    before :each do
+      @user = create :user, activated: false, activated_at: nil
+    end
 
+    it "does not activate with invalid token" do
+      get :edit, id: "invalid", email: @user.email
+      expect(@user.activated?).to be(false)
+      expect(session[:user_id]).to be(nil)
+    end
+
+    it "does not activate with valid token and invalid email" do
+      get :edit, id: @user.activation_token, email: 'wrong'
+      expect(@user.activated?).to be(false)
+      expect(session[:user_id]).to be(nil)
+    end
+
+    it "activates user with valid token and valid email" do
+      get :edit, id: @user.activation_token, email: @user.email
+      expect(session[:user_id])
+      expect(@user.activated?)
+    end
+  end
 end
