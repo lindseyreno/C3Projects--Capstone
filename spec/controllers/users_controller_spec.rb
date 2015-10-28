6c97b54d282cd63eab693e7d1df0c6e5def53fd7
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-28 12:25:50 -0700

    removed unneeded tests and added tests for #update

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,36 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe 'GET #new' do
-    context 'user not logged in' do
-      it 'responds successfully with an HTTP 200 status code' do
-        get :new
-        expect(response).to be_success
-        expect(response).to have_http_status(200)
-      end
-
-      it 'renders the new view' do
-        get :new
-        expect(response).to render_template('new')
-      end
-    end
-
-    context 'user logged in' do
-      before :each do
-        @user = create :user
-        session[:user_id] = @user.id
-        get :new
-      end
-
-      it 'redirects logged in user to home page' do
-        expect(response).to redirect_to(root_path)
-      end
-
-      it 'flashes error message' do
-        expect(flash[:errors]).to include('You already have an account.')
-      end
-    end
-  end
 
   describe 'POST #create' do
     context 'valid user params' do
@@ -62,10 +32,10 @@ RSpec.describe UsersController, type: :controller do
         expect(flash[:errors]).to eq('There was a problem with your sign up info. Please try again.')
       end
 
-      it 'renders the new view' do
+      it 'redirects to the home page' do
         post :create, user: attributes_for(:user, username: nil)
 
-        expect(response).to render_template(:new)
+        expect(response).to redirect_to(root_path)
       end
     end
   end
@@ -180,6 +150,65 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
+  describe 'PATCH #update' do
+    describe 'user not signed in' do
+      before :each do
+        user = create :user
+        patch :update, id: user
+      end
+
+      it 'redirects to the home page' do
+        expect(subject).to redirect_to(root_path)
+      end
+
+      it 'flashes an error message' do
+        expect(flash[:errors]).to include('You must be signed in to view this page.')
+      end
+    end
+
+    describe 'user signed in' do
+      context 'valid user_params' do
+        before :each do
+          @user = create :user
+          session[:user_id] = @user.id
+          patch :update, id: @user, user: { username: 'sam', email: 'sammy@user.com', password: 'user_password', password_confirmation: 'user_password' }
+          @user = assigns(:user)
+        end
+
+        it 'updates the user record' do
+          user = User.find(session[:user_id])
+          expect(@user.email).to eq user.email
+        end
+
+        it 'flashes a success message' do
+          expect(flash[:messages]).to include 'Changes successfully saved.'
+        end
+
+        it 'redirects to the home page' do
+          expect(response).to redirect_to root_path
+        end
+      end
+
+      context 'invalid user_params' do
+        before :each do
+          @user = create :user
+          session[:user_id] = @user.id
+          patch :update, id: session[:user_id], user: { username: 'sam', email: nil, password: 'user_password', password_confirmation: 'user_password' }
+          @user = assigns(:user)
+        end
+
+        it 'does not update the user record' do
+          user = User.find(session[:user_id])
+          expect(@user.email).to_not eq user.email
+        end
+
+        it 'renders the edit page' do
+          expect(response).to render_template('edit', session[:user_id])
+        end
+      end
+    end
+  end
+
   describe 'PATCH #update_preferences' do
     describe 'user not signed in' do
       before :each do
@@ -197,38 +226,47 @@ RSpec.describe UsersController, type: :controller do
     end
 
     describe 'user signed in' do
-      before :each do
-        @user = create :user
-        @category = create :category
-        @user.categories = [@category]
-        session[:user_id] = @user.id
-      end
+      context 'valid user_params' do
+        before :each do
+          @user = create :user
+          @category = create :category
+          @user.categories = [@category]
+          session[:user_id] = @user.id
+        end
 
-      it 'unassociates the user from all categories when no checkboxes are checked' do
-        patch :update_preferences, id: @user
-        @user = assigns(:user)
+        it 'unassociates the user from all categories when no checkboxes are checked' do
+          patch :update_preferences, id: @user
+          @user = assigns(:user)
 
-        expect(@user.categories).to eq []
-      end
+          expect(@user.categories).to eq []
+        end
 
-      it 'associates a category with the user when a checkbox is checked' do
-        category2 = create :category, name: 'Film'
+        it 'associates a category with the user when a checkbox is checked' do
+          category2 = create :category, name: 'Film'
 
-        patch :update_preferences, id: @user, user: { category_ids: [@category.id, category2.id] }
-        @user = assigns(:user)
+          patch :update_preferences, id: @user, user: { category_ids: [@category.id, category2.id] }
+          @user = assigns(:user)
 
-        expect(@user.categories.length).to eq 2
-        expect(@user.categories).to include(category2)
-      end
+          expect(@user.categories.length).to eq 2
+          expect(@user.categories).to include(category2)
+        end
 
-      it 'associates a schedule with a user' do
-        schedule = create :schedule
+        it 'associates a schedule with a user' do
+          schedule = create :schedule
 
-        patch :update_preferences, id: @user, schedule_id: schedule.id
-        @user = assigns(:user)
+          patch :update_preferences, id: @user, schedule_id: schedule.id
+          @user = assigns(:user)
 
-        expect(@user.schedule_id).to eq schedule.id
-        expect(Schedule.find(@user.schedule_id).name).to eq 'Every Day'
+          expect(@user.schedule_id).to eq schedule.id
+          expect(Schedule.find(@user.schedule_id).name).to eq 'Every Day'
+        end
+
+        it 'flashes a success message' do
+          patch :update_preferences, id: @user
+          @user = assigns(:user)
+
+          expect(flash[:messages]).to include 'Changes successfully saved.'
+        end
       end
     end
   end
