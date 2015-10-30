c10a2664bae068530518d77e9e6a25cba781c500
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-30 10:00:11 -0700

    put sendgrid back in

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,13 +66,23 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'myseattleevents.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
+  # ActionMailer::Base.smtp_settings = {
+  #   :address        => ENV['MAILGUN_SMTP_SERVER'],
+  #   :port           => ENV['MAILGUN_SMTP_PORT'],
+  #   :authentication => :plain,
+  #   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  #   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  #   :domain         => 'myseattleevents.herokuapp.com',
+  #   :enable_starttls_auto => true
+  # }
+
   ActionMailer::Base.smtp_settings = {
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'myseattleevents.herokuapp.com',
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 
