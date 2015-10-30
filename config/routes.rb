6c10fb0aea4740853a0212dfff913828e56b46bd
Lindsey Reno <lindseyannereno@gmail.com>
2015-10-30 02:27:48 -0700

    added route for categories show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  resources :users
 
   get '/sign_in', to: 'sessions#new', as: 'sign_in'
   post '/sign_in', to: 'sessions#create'
@@ -9,5 +8,7 @@ Rails.application.routes.draw do
   get '/users/:id/edit_preferences', to: 'users#edit_preferences', as: 'edit_preferences'
   patch '/users/:id/update_preferences', to: 'users#update_preferences', as: 'update_preferences'
 
+  resources :users
   resources :account_activations, only: [:edit]
+  resources :categories, only: [:show]
 end
